9e1e2f86a7729ef4e159b4c2418dea8c5a2613f1
Rostyslav Golda <tx_hv@ukr.net>
2018-09-05 16:05:13 +0300

    Corrected linux script. Added start- scripts

--- update1.sh
+++ update1.sh
@@ -3,7 +3,7 @@
 # first parameter is a current directory, where wallet is executing now (directory, which we should update)
 # second parameter is a update directory which contains unpacked jar for update
 # third parameter is a boolean flag, which indicates desktop mode
-if  [[ -d $1  && [ -d $2 ] && [ -n $3 ]]
+if  [ -d $1 ] && [ -d $2 ] && [ -n $3 ]
 then
     echo Starting Platform Dependent Updater
     echo Stopping wallet.... 
