8b7086b3a2e21f3cdb0dc958e747866546f3653f
Rostyslav Golda <tx_hv@ukr.net>
2018-09-06 12:36:44 +0300

    fixed linux script

--- update1.sh
+++ update1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # first parameter is a current directory, where wallet is executing now (directory, which we should update)
 # second parameter is a update directory which contains unpacked jar for update
@@ -9,21 +9,25 @@ then
     echo Stopping wallet.... 
     NEXT_WAIT_TIME=0
     
-    until [ $(ps aux | grep Apollo.jar | wc -l) -eq 0 ] || [ $NEXT_WAIT_TIME -eq 10 ]; do
-	sleep `expr $NEXT_WAIT_TIME '+' 1`
+    until [ $(ps aux | grep Apollo.jar | grep -v grep | wc -l) -eq 0 ] || [ $NEXT_WAIT_TIME -eq 10 ]; do
+	NEXT_WAIT_TIME=`expr $NEXT_WAIT_TIME '+' 1`
+	sleep $NEXT_WAIT_TIME
 	echo "Waiting more time to stop wallet..."
     done
 
     echo Copy update files
     cp -TRa $2 $1
 
+    cd $1 
+    chmod 755 *.sh
+
     if [ $3 == true ]
     then
         echo Start desktop application
-        cd $1 && exec sh start-desktop.sh
+        ./start-desktop.sh
     else
         echo Start command line application
-        cd $1 && exec sh start.sh
+        ./start.sh
     fi
 
 else
