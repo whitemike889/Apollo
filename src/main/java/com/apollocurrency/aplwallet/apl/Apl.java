da8f45dda6ea95d8be07ec6811610eed832102a9
Rostyslav Golda <tx_hv@ukr.net>
2018-11-23 15:35:52 +0200

    1.21.19

--- src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
@@ -75,7 +75,7 @@ import org.slf4j.Logger;
 public final class Apl {
     private static Logger LOG;
     private static ChainIdService chainIdService;
-    public static final Version VERSION = Version.from("1.21.18");
+    public static final Version VERSION = Version.from("1.21.19");
     public static final String APPLICATION = "Apollo";
     private static Thread shutdownHook;
     private static volatile Time time = new Time.EpochTime();
